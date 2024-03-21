226286f2620b9c8e95ca810aeed15bf1eca3e9e9
Manuel García-Ulloa <36465310+manuelgug@users.noreply.github.com>
2024-03-21 11:22:55 +0100

    working prototype

--- CNV_new_approach.R
+++ CNV_new_approach.R
@@ -1,11 +1,24 @@
 library(ggplot2)
+library(ggpubr)
 library(dplyr)
 
 
-data <- read.table("CNV_runs_sample_coverage/BOH22_NextSeq01_amplicon_coverage_DD2_PM2-2.txt", header = T)
+data <- read.table("CNV_runs_sample_coverage/REACT2_NextSeq01_amplicon_coverage_DD2.txt", header = T)
 data <- data[,-3:-4]
 
-#normalize read count for each sample
+#remove neg controls and undetermined
+data <- data[!grepl("(?i)neg", data$SampleID), ]
+data <- data[!grepl("(?i)Undetermined", data$SampleID), ]
+
+#samples to be flagged as low quality (below a median of 100 read count)
+co <- data %>%
+  group_by(SampleID)%>%
+  summarize(median_read_count = median(OutputPostprocessing))
+
+samples_below_median_100 <- co[co$median_read_count < 100,]$SampleID 
+
+
+# calculate proportions of amplicons for each sample
 data_norm <- data %>% 
   group_by(SampleID) %>%
   summarize(NORM_OutputPostprocessing = OutputPostprocessing / sum(OutputPostprocessing))
@@ -13,7 +26,7 @@ data_norm <- data %>%
 data_norm <- as.data.frame(cbind(data_norm, Locus = data$Locus))
 
 
-# add loci of interest
+# add loci of interest column
 loci_of_interest <- readRDS("loci_of_interest.RDS")
 
 loi <- character(nrow(data))
@@ -30,14 +43,14 @@ for (i in seq_len(nrow(data_norm))) {
   }
 }
 
-# Add the 'loi' column to the dataframe
 data_norm$loi <- loi
+
 
 #extract single-copy controls
 controls <- data_norm[!grepl("(?i)Dd2|PM|HB3", data_norm$SampleID) & grepl("(?i)3D7", data_norm$SampleID), ]
 controls_nozero <- controls[controls$NORM_OutputPostprocessing != 0, ]
 
-#calculate thresholds deom single-copy controls
+#calculate thresholds from single-copy controls
 thresholds_controls<- controls_nozero %>% 
   group_by(SampleID) %>%
   summarize(q_up = quantile(NORM_OutputPostprocessing,0.99),
@@ -46,13 +59,32 @@ thresholds_controls<- controls_nozero %>%
             min = min(NORM_OutputPostprocessing))
 
 
+# ##############################################################
+#find best control (most reads total)
+# controls_raw <- data[!grepl("(?i)Dd2|PM|HB3", data$SampleID) & grepl("(?i)3D7", data$SampleID), ]
+# 
+# total_read_counts <- controls_raw %>%
+#   group_by(SampleID) %>%
+#   summarize(read_count_total = sum(OutputPostprocessing))
+# 
+# best_control <- total_read_counts[which.max(total_read_counts$read_count_total),][1]
+# 
+# 
+# #normalize proportions using best control
+# best_control_propotions <- controls_nozero[controls_nozero$SampleID ==best_control$SampleID,]
+# 
+# best_control_propotions
+# data_norm
+# 
+# merged_table<- merge(data_norm, best_control_propotions[c("Locus", "NORM_OutputPostprocessing")], by ="Locus")
+# 
+# merged_table$fold_change_probs<- merged_table$NORM_OutputPostprocessing.x / merged_table$NORM_OutputPostprocessing.y
+##############################################################
+
+# VISUALIZATION #
 
 #grab a sample
-unique_samples <- unique(data_norm$SampleID)
-
-xsample <- unique_samples[251]
-
-sample <- data_norm[data_norm$SampleID == xsample , ] #& data_norm$NORM_OutputPostprocessing != 0
+unique_samples <- sort(unique(merged_table$SampleID))
 
 # Calculate the median of OutputPostprocessing
 up_threshold_q <- mean(thresholds_controls$q_up)
@@ -60,18 +92,50 @@ down_threshold_q <- mean(thresholds_controls$q_down)
 up_threshold <- max(thresholds_controls$max)
 down_threshold <- min(thresholds_controls$min)
 
-# Scatterplot with sorted samples and median line
-ggplot(sample, aes(x = reorder(Locus, -NORM_OutputPostprocessing), y = log(NORM_OutputPostprocessing), color = loi)) +
-  geom_point(size=4, alpha =0.7) +
-  geom_hline(yintercept = log(up_threshold), linetype = "dashed", color = "red") +
-  geom_hline(yintercept = log(up_threshold_q), linetype = "dashed", color = "blue") +
-  labs(x = "Locus", y = "Read Count", title = xsample) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6.5))
+
+# Initialize an empty list to store plots
+plot_list <- list()
+
+# Loop through each sample in unique_samples
+for (xsample in unique_samples) {
+  # Subset the data for the current sample
+  sample <- data_norm[data_norm$SampleID == xsample, ]
+  
+  # Create scatterplot
+  p <- ggplot(sample, aes(x = reorder(Locus, -NORM_OutputPostprocessing), y = NORM_OutputPostprocessing, color = loi)) +
+    geom_point(size=4, alpha =0.7) +
+    geom_hline(yintercept = up_threshold, linetype = "dashed", color = "red") +
+    #geom_hline(yintercept = 2, linetype = "dashed", color = "red") +
+    labs(x = "Locus", y = "Read Proportions", title = xsample) +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6.5))
+  
+  # Add the plot to the list
+  plot_list[[length(plot_list) + 1]] <- p
+}
+
+# Arrange plots in a grid
+grid <- ggarrange(plotlist = plot_list[[1:9]], nrow = 3, ncol =3 )
 
 
+
+# ACTUAL RESULTS #
 
 # quantitative
 data_norm$CNV_result <- ifelse(data_norm$NORM_OutputPostprocessing > max(thresholds_controls$max), "CNV", "single")
+data_norm$QC<- ifelse(data_norm$SampleID %in% samples_below_median_100, "median_below_100", "good")
+
+#check results: good QC samples that have CNV for loci of interest
+results <- data_norm[!is.na(data_norm$loi)& data_norm$CNV_result == "CNV" & data_norm$QC == "good",]
+
+length(unique(results$SampleID))
+
+# # quantitative
+# merged_table$CNV_result <- ifelse(merged_table$fold_change_probs > 2, "CNV", "single")
+# merged_table$QC<- ifelse(merged_table$SampleID %in% samples_below_median_100, "median_below_100", "good")
+# 
+# #check results: good QC samples that have CNV for loci of interest
+# results <- merged_table[!is.na(merged_table$loi)& merged_table$CNV_result == "CNV" & merged_table$QC == "good",]
+
 
 
 # NOTES
