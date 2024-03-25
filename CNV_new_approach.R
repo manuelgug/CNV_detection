1a517b67d537d7c827842a1e7cf1f60bdd6088e9
Manuel García-Ulloa <36465310+manuelgug@users.noreply.github.com>
2024-03-25 16:58:59 +0100

    TONS of threshold adjustments. CHECKPOINT!

--- CNV_new_approach.R
+++ CNV_new_approach.R
@@ -4,6 +4,23 @@ library(dplyr)
 library(cowplot)
 
 
+###
+# parameters to tune:
+
+# 1) amount of amplicons to keep for the slope analysis
+# sample_slope[(length(sample_slope$y) - 29):length(sample_slope$y), ]
+
+# 2) quantiles to use as thresholds
+# q_up <- quantile(sample_slopes$Slope, probs = 0.95) 
+# q_down <- quantile(sample_slopes$Slope, probs = 0.05)
+
+# 3) min or mean max proportion as threshold from single-copy controls
+# up_threshold <- min(thresholds_controls$max)
+
+# 4) amplicons to exclude
+
+####
+
 data <- read.table("CNV_runs_sample_coverage/HFS_NextSeq01_amplicon_coverage_DD2.txt", header = T)
 data <- data[,-3:-4]
 
@@ -66,9 +83,13 @@ for (sample_id in unique_samples) {
 shapiro_test <- shapiro.test(sample_slopes$Slope)
 p_value <- shapiro_test$p.value #not significant means normal means solid run
 
-# Calculate quantiles
-q_up <- quantile(sample_slopes$Slope, probs = 0.95) #samples with many amplicons at 0 (MAYBE HERE'S NO NEED TO REMOVE THESE?)
-q_down <- quantile(sample_slopes$Slope, probs = 0.05) #samples with overall low yield or 
+# # Calculate quantiles
+# q_up <- quantile(sample_slopes$Slope, probs = 0.99) #samples with many amplicons at 0 (MAYBE HERE'S NO NEED TO REMOVE THESE?)
+# q_down <- quantile(sample_slopes$Slope, probs = 0.05) #samples with overall low yield or 
+
+# # calculate sds
+ sd_down <- mean(sample_slopes$Slope) - sd(sample_slopes$Slope)*2
+ sd_up <- mean(sample_slopes$Slope) + sd(sample_slopes$Slope)*2
 
 histogram_slopes <- ggplot(sample_slopes, aes(x = Slope)) +
   geom_histogram(bins = 150, fill = "skyblue", color = "skyblue", alpha = 0.7) +
@@ -76,14 +97,14 @@ histogram_slopes <- ggplot(sample_slopes, aes(x = Slope)) +
   theme_minimal()+
   annotate("text", x = Inf, y = Inf, label = paste("Shapiro-Wilk p-value:", round(p_value, 4)), 
            hjust = 1, vjust = 1, size = 4)+
-  geom_vline(xintercept = q_down, col = "red", linetype = "dashed") +
-  geom_vline(xintercept = q_up, col = "blue", linetype = "dashed")
+  geom_vline(xintercept = sd_up, col = "blue", linetype = "dashed") +
+  geom_vline(xintercept = sd_down, col = "red", linetype = "dashed")
 
 histogram_slopes
 
 sample_slopes <- sample_slopes[order(sample_slopes$Slope), ]
 
-bad_slope_samples <- sample_slopes[sample_slopes$Slope > q_up | sample_slopes$Slope < q_down,]$SampleID
+bad_slope_samples <- sample_slopes[sample_slopes$Slope > sd_up | sample_slopes$Slope < sd_down,]$SampleID
 
 
 
@@ -124,7 +145,7 @@ ranking_amps <- ranking_amps%>%
 controls_nozero <- controls_nozero[!controls_nozero$Locus %in% ranking_amps$Locus[1:5],]
 
 #exclude some HRP3 and PM2 ampliconS that too outlierish:
-controls_nozero <- controls_nozero[!controls_nozero$Locus %in% c("Pf3D7_13_v3-2816310-2816554-1B","Pf3D7_13_v3-2814583-2814832-2", "Pf3D7_14_v3-294506-294753-1B"),]
+controls_nozero <- controls_nozero[!controls_nozero$Locus %in% c("Pf3D7_13_v3-2816310-2816554-1B","Pf3D7_13_v3-2814583-2814832-2", "Pf3D7_14_v3-294506-294753-1B", "Pf3D7_13_v3-2814606-2814855-1B"),]
 
 #calculate thresholds from single-copy controls
 thresholds_controls<- controls_nozero %>% 
@@ -132,13 +153,29 @@ thresholds_controls<- controls_nozero %>%
   summarize(max = max(NORM_OutputPostprocessing),
             min = min(NORM_OutputPostprocessing))
 
+# remove outliers by IQR method
+Q1 <- quantile(thresholds_controls$max, 0.25)
+Q3 <- quantile(thresholds_controls$max, 0.75)
 
+IQR <- Q3 - Q1
 
+lower_bound <- Q1 - 1.5 * IQR
+upper_bound <- Q3 + 1.5 * IQR
+
+thresholds_controls <- thresholds_controls[thresholds_controls$max >= lower_bound & thresholds_controls$max <= upper_bound,]
+
+#keep top controls with max > 0.01 when possible
+if (any(thresholds_controls$max >= 0.01)){
+  thresholds_controls <- thresholds_controls[thresholds_controls$max >= 0.01,]
+}
+
+ 
+  
 # remove top 5 most abundant amplicons from data_norm (TO AVOID FALSE POSITIVES)
 data_norm <- data_norm[!data_norm$Locus %in% ranking_amps$Locus[1:5],]
 
 #exclude some HRP3 and PM2 ampliconS that too outlierish:
-data_norm <- data_norm[!data_norm$Locus %in% c("Pf3D7_13_v3-2816310-2816554-1B","Pf3D7_13_v3-2814583-2814832-2", "Pf3D7_14_v3-294506-294753-1B"),]
+data_norm <- data_norm[!data_norm$Locus %in% c("Pf3D7_13_v3-2816310-2816554-1B","Pf3D7_13_v3-2814583-2814832-2", "Pf3D7_14_v3-294506-294753-1B", "Pf3D7_13_v3-2814606-2814855-1B"),]
 
 
 #data_norm[data_norm$SampleID == "N1979742_7_S187" & !is.na(data_norm$loi) ,]
@@ -166,43 +203,16 @@ data_norm <- data_norm[!data_norm$Locus %in% c("Pf3D7_13_v3-2816310-2816554-1B",
 # merged_table$fold_change_probs<- merged_table$NORM_OutputPostprocessing.x / merged_table$NORM_OutputPostprocessing.y
 ##############################################################
 
+# RESULTS #
 
-# VISUALIZATION #
+# Set threshold for CNV
+if (any(thresholds_controls$max >= 0.01)){
+  up_threshold <- min(thresholds_controls$max)
+}else{
+  up_threshold <- max(thresholds_controls$max)
+  }
 
-# Set thresholds
-#up_threshold <- max(thresholds_controls$max)
-up_threshold_mean <- mean(thresholds_controls$max)
-
-
-plot_list <- list()
-
-# Loop through each sample in unique_samples
-for (xsample in unique_samples) {
-  # Subset the data for the current sample
-  sample <- data_norm[data_norm$SampleID == xsample, ]
-  
-  # Create scatterplot
-  p <- ggplot(sample, aes(x = reorder(Locus, -NORM_OutputPostprocessing), y = NORM_OutputPostprocessing, color = loi)) +
-    geom_point(size=4, alpha =0.7) +
-    geom_hline(yintercept = up_threshold_mean, linetype = "dashed", color = "red") +
-    #geom_hline(yintercept = 2, linetype = "dashed", color = "red") +
-    labs(x = "Locus", y = "Read Proportions", title = xsample) +
-    #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6.5))+
-    theme(axis.text.x = element_blank(),  # Remove amplicon names
-          axis.title.x = element_blank())
-  
-  # Add the plot to the list
-  plot_list[[length(plot_list) + 1]] <- p
-}
-
-# Assuming 'plot_list' contains a list of ggplot objects
-combined_plot <- plot_grid(plotlist = plot_list, nrow = ceiling(sqrt(length(unique_samples))), ncol = ceiling(sqrt(length(unique_samples))))
-ggsave("grid_of_plots.pdf", plot = combined_plot, width = 60, height = 50, dpi = 300, limitsize = FALSE)
-
-# ACTUAL RESULTS #
-
-# quantitative
-data_norm$CNV_result <- ifelse(data_norm$NORM_OutputPostprocessing > up_threshold_mean, "CNV", "single")
+data_norm$CNV_result <- ifelse(data_norm$NORM_OutputPostprocessing > up_threshold, "CNV", "single")
 data_norm$QC<- ifelse(data_norm$SampleID %in% samples_below_median_100, "bad", "good")
 data_norm$slope<- ifelse(data_norm$SampleID %in% bad_slope_samples, "bad", "good")
 
@@ -220,6 +230,34 @@ print(paste0("There are ", length(unique(results$SampleID)), " samples with good
 
 write.csv(results, "CNV_results.csv", row.names = F)
 
+
+
+# VISUALIZATION #
+
+plot_list <- list()
+
+# Loop through each sample in unique_samples
+for (xsample in unique_samples) {
+  # Subset the data for the current sample
+  sample <- data_norm[data_norm$SampleID == xsample, ]
+  
+  # Create scatterplot
+  p <- ggplot(sample, aes(x = reorder(Locus, -NORM_OutputPostprocessing), y = NORM_OutputPostprocessing, color = loi)) +
+    geom_point(size=4, alpha =0.7) +
+    geom_hline(yintercept = up_threshold, linetype = "dashed", color = "red") +
+    #geom_hline(yintercept = 2, linetype = "dashed", color = "red") +
+    labs(x = "Locus", y = "Read Proportions", title = xsample) +
+    #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1, size = 6.5))+
+    theme(axis.text.x = element_blank(),  # Remove amplicon names
+          axis.title.x = element_blank())
+  
+  # Add the plot to the list
+  plot_list[[length(plot_list) + 1]] <- p
+}
+
+# Assuming 'plot_list' contains a list of ggplot objects
+combined_plot <- plot_grid(plotlist = plot_list, nrow = ceiling(sqrt(length(unique_samples))), ncol = ceiling(sqrt(length(unique_samples))))
+ggsave("grid_of_plots.pdf", plot = combined_plot, width = 60, height = 50, dpi = 300, limitsize = FALSE)
 
 
 # NOTES
